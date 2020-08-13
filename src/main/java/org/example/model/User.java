0253c97f38ef3d91cc4eb651a0aa4e0e894bf90e
Elitebook <ion.isac94@gmail.com>
2020-08-13 15:23:05 +0300

    refactored code with some improv

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -1,5 +1,8 @@
 package org.example.model;
 
+import lombok.Data;
+
+@Data
 public class User {
 
 	private String username;
@@ -9,49 +12,4 @@ public class User {
 	private String email;
 	private String address;
 	private int phone;
-
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getFirstname() {
-		return firstname;
-	}
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
-	public String getLastname() {
-		return lastname;
-	}
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public int getPhone() {
-		return phone;
-	}
-	public void setPhone(int phone) {
-		this.phone = phone;
-	}
-
-
 }
